1320949f4b5b6789d78efea2ec2d8a5ab9695bdf
Max Hissen <maxhissen1@gmail.com>
2024-10-22 16:56:38 -0700

    Add files via upload

--- PracticeBots/RandomBot/main.py
+++ PracticeBots/RandomBot/main.py
@@ -1,1 +1,28 @@
+import random
+import typing
 
+def move(game_state: typing.Dict) -> int:
+    next_move = 4
+
+    print(f"MOVE: {next_move}")
+    return {"move": next_move}
+    
+
+def info() -> typing.Dict:
+    print("INFO")
+    return {
+        "apiversion": "1",
+        "author": "Snoblaxx",
+        "name": "Column5Bot",
+        "info": "This bot really likes the number 5"
+    }
+
+
+# Start server when `python main.py` is run
+if __name__ == "__main__":
+    from server import run_server
+
+    run_server({
+        "move": move,
+        "info": info
+    })
