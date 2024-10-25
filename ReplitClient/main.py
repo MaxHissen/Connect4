2b1d4a14366c5fadeb4cfaf1eba3cd264d3dd647
Max Hissen <maxhissen1@gmail.com>
2024-10-25 00:10:25 -0700

    Update main.py

--- ReplitClient/main.py
+++ ReplitClient/main.py
@@ -1,19 +1,24 @@
 import random
 import typing
+import ast
+
+from get_move import get_move
 
 
 
 # move is called on every turn and returns your next move
 # Valid moves are from 0-6
 # returns what column to put move in
-# game_state: dict of 1 = my moves, -1 = opponent moves, 0 = empty
+# game_state: dict. 1 -> my moves, -1 -> opponent moves, 0 -> empty
 def move(game_state: typing.Dict) -> int:
 
     # TODO: Implement move logic here
-    next_move = 3
+    board = [[0]*7 for i in range(6)]
+    for key in game_state:
+        x,y = ast.literal_eval(key)
+        board[int(y)][int(x)] = game_state[key]
 
-
-    
+    next_move = get_move(board)
     print(f"MOVE: {next_move}")
     return {"move": next_move}
     
@@ -24,8 +29,8 @@ def info() -> typing.Dict:
     return {
         "apiversion": "1",
         "author": "Snoblaxx",  # TODO: Your Connect4 AI Username
-        "name": "Column4Bot",
-        "info": "This bot really likes the number 4"
+        "name": "TODO",
+        "info": "TODO"
     }
 
 
