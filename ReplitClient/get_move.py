a6c19e9d0314e3e599b000a6035216105f3c94a4
Max Hissen <maxhissen1@gmail.com>
2024-10-25 00:12:23 -0700

    Update get_move.py

--- ReplitClient/get_move.py
+++ ReplitClient/get_move.py
@@ -1,8 +1,15 @@
+"""
+prints current board state
+"""
 def print_board(board):
   for y in range(len(board)):
     print(board[len(board) - y - 1])
 
+"""
+returns move to be made
+"""
 def get_move(board):
+  #TODO: Implement move logic here
   print_board(board)
   
   return 1
