b959d7a1e4a7888f085a92c29d057934a63662be
Max Hissen <maxhissen1@gmail.com>
2024-10-25 14:15:21 -0700

    Update Main.py
    added DummyABPruner

--- Server/Main.py
+++ Server/Main.py
@@ -9,31 +9,27 @@ DISPLAY = True
 PLAYER_DICT = {
     "Column5Bot" : "https://connect4-ydyh.onrender.com/",
     "RandomBot" : "https://connect4-1.onrender.com/",
-    "CounterBot" : "https://connect4-2.onrender.com/"
+    "CounterBot" : "https://connect4-2.onrender.com/",
+    "DummyABPruner" : "https://connect4-tbp7.onrender.com/"
 }
 
 
 """
 TestGame to test your AI.
-
-Takes:
-string: playerX, string: playerY, bool:DISPLAY_BOARD=False
 """
-player_1 = PLAYER_DICT["RandomBot"]
-player_2 = PLAYER_DICT["RandomBot"]
+player_1 = "https://36bfd9f0-58b2-439d-9b8c-0d9542714e51-00-30ivmv1v0r0jy.kirk.replit.dev/" #X
+player_2 = "https://36bfd9f0-58b2-439d-9b8c-0d9542714e51-00-30ivmv1v0r0jy.kirk.replit.dev/" #Y
 #TestGame(player_1, player_2, display=DISPLAY)
 
 
 """
 Tournament
-
-takes
-list[string] players, bool:DISPLAY_GAMES=False
 """
 players = []
 players.append(PLAYER_DICT["RandomBot"])
 players.append(PLAYER_DICT["Column5Bot"])
 players.append(PLAYER_DICT["CounterBot"])
+players.append(PLAYER_DICT["DummyABPruner"])
 
 #Tournament settings
 wins = 4 #first to n wins advances
@@ -41,7 +37,7 @@ home_away_schedule = [1,1,0,0,1,0,1] #higher seed goes first if 1
 
 
 #Round Robin settings
-games = 2 #games per player in season. Will be rounded up to nearest even number. Recommended to be a multiple of amount of players
+games = 6 #games per player in season. Will be rounded up to nearest even number. Recommended to be a multiple of amount of players
 #RunTournament(players_list=players, wins_to_clinch_series=wins, home_away_schedule=home_away_schedule, season_games_amount=games)
 
 
